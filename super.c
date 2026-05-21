5e57aa96587a4a7829161b28ea8d1228d1164b1e
max7im <nogagorilli@gmail.com>
2026-05-22 01:16:25 +0300

    fix

--- super.c
+++ super.c
@@ -1,6 +1,5 @@
 #include "simplefs.h"
 
-#include <linux/bdev.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/string.h>
@@ -107,7 +106,8 @@ int simplefs_load_or_format(struct super_block *sb,
   u64 file_bytes;
   int ret;
 
-  dev_sectors = bdev_nr_sectors(sb->s_bdev);
+  loff_t size = i_size_read(sb->s_bdev->bd_inode);
+  dev_sectors = size >> 9;
   sbi->total_sectors = dev_sectors;
 
   if (sbi->super1_sector == sbi->super2_sector)
