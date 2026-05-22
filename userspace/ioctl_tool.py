14e97c7db6f25540ce01a8e87158ceb3c272afda
max7im <nogagorilli@gmail.com>
2026-05-23 00:16:07 +0300

    fix ioctl_tol

--- userspace/ioctl_tool.py
+++ userspace/ioctl_tool.py
@@ -1,21 +1,27 @@
-#!/usr/bin/env python3
-import os
-import fcntl
 import argparse
+import fcntl
 import struct
 
-# должен совпадать с kernel header
+# ===== must match kernel =====
 SIMPLEFS_IOC_MAGIC = ord('s')
 
-IOCTL_CLEAR = 0x00  # _IO(s, 1)
-IOCTL_ERASE = 0x01  # _IO(s, 2)
-IOCTL_GET_HASHES = 0xC0107303  # _IOWR(s, 3, struct simplefs_hashes_hdr)
-IOCTL_GET_MAP = 0xC0287304      # _IOWR(s, 4, struct simplefs_map_req)
+
+def _io(magic, nr):
+    return (magic << 8) | nr
 
 
-HASH_HDR_FMT = "II"  # capacity, count
+IOCTL_CLEAR = _io(SIMPLEFS_IOC_MAGIC, 1)
+IOCTL_ERASE = _io(SIMPLEFS_IOC_MAGIC, 2)
 
-MAP_FMT = f"{128}sQIIQ"  # name, start, sectors, hash, size
+# _IOWR in Linux:
+
+
+def _iowr(magic, nr, size):
+    return (magic << 8) | (nr) | (size << 16)
+
+
+IOCTL_GET_HASHES = _iowr(SIMPLEFS_IOC_MAGIC, 3, 8)
+IOCTL_GET_MAP = _iowr(SIMPLEFS_IOC_MAGIC, 4, 128)
 
 
 def ioctl_clear(fd):
@@ -26,67 +32,26 @@ def ioctl_erase(fd):
     fcntl.ioctl(fd, IOCTL_ERASE)
 
 
-def ioctl_get_hashes(fd, capacity):
-    buf = struct.pack(HASH_HDR_FMT, capacity, 0)
-    res = fcntl.ioctl(fd, IOCTL_GET_HASHES, buf)
-
-    capacity, count = struct.unpack(HASH_HDR_FMT, res[:8])
-    hashes = struct.unpack(f"{count}I", res[8:8 + count * 4])
-    return hashes
-
-
-def ioctl_get_map(fd, name):
-    name_b = name.encode()[:127]
-    name_b = name_b + b"\x00" * (128 - len(name_b))
-
-    buf = struct.pack(MAP_FMT, name_b, 0, 0, 0, 0)
-    res = fcntl.ioctl(fd, IOCTL_GET_MAP, buf)
-
-    name, start, sectors, hashv, size = struct.unpack(MAP_FMT, res)
-
-    return {
-        "name": name.decode(errors="ignore").rstrip("\x00"),
-        "start": start,
-        "sectors": sectors,
-        "hash": hashv,
-        "size": size,
-    }
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dev", required=True)
-    sub = parser.add_subparsers(dest="cmd")
-
-    sub.add_parser("clear")
-    sub.add_parser("erase")
-
-    p_hash = sub.add_parser("hashes")
-    p_hash.add_argument("--cap", type=int, default=1024)
-
-    p_map = sub.add_parser("map")
-    p_map.add_argument("name")
+    parser.add_argument("cmd")
 
     args = parser.parse_args()
 
-    fd = os.open(args.dev, os.O_RDWR)
+    with open(args.dev, "rb+", buffering=0) as f:
+        fd = f.fileno()
 
-    if args.cmd == "clear":
-        ioctl_clear(fd)
+        if args.cmd == "clear":
+            ioctl_clear(fd)
+            print("OK clear")
 
-    elif args.cmd == "erase":
-        ioctl_erase(fd)
+        elif args.cmd == "erase":
+            ioctl_erase(fd)
+            print("OK erase")
 
-    elif args.cmd == "hashes":
-        print(ioctl_get_hashes(fd, args.cap))
-
-    elif args.cmd == "map":
-        print(ioctl_get_map(fd, args.name))
-
-    else:
-        print("unknown command")
-
-    os.close(fd)
+        else:
+            print("unknown cmd")
 
 
 if __name__ == "__main__":
